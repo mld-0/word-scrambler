ed5e05bbc3824e64c7af139379a9ed46c410f235
mld-0 <mld.0@protonmail.com>
2023-05-25 21:39:40 +1000

    preserve leading/trailing whitespace

--- word-scrambler.py
+++ word-scrambler.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import string
+import re
 
 def scramble_word(word):
     """This function takes in a word and returns a new word with randomly replaced characters if the word contains only lowercase letters"""
@@ -9,12 +10,26 @@ def scramble_word(word):
     else:
         return word
 
-def main():
-    """This function takes in standard input, breaks it into words, and applies the scramble_word function to each word"""
-    for line in sys.stdin:
+def scramble_input(f):
+    """This function takes a stream, breaks it into words, and applies the scramble_word function to each word"""
+    for line in f:
+        leading_whitespace = ''
+        trailing_whitepace = ''
+        t = re.search(r'^\s*', line)
+        if t:
+            leading_whitespace = t.group(0)
+        t = re.search(r'\s*$', line)
+        if t:
+            trailing_whitepace = t.group(0)
         words = line.split()
         scrambled_words = [scramble_word(word) for word in words]
-        print(' '.join(scrambled_words))
+        result = leading_whitespace + ' '.join(scrambled_words) + trailing_whitepace
+        print(result)
+
+
+def main():
+    scramble_input(sys.stdin)
+
 
 if __name__ == "__main__":
     main()
